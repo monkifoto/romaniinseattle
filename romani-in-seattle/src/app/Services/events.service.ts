ab1bece4538b7bff4701364666196112517570eb
Monkifoto LLc <monkifoto@gmail.com>
2024-05-23 16:47:06 -0700

    Added Events

---
+++ romani-in-seattle/src/app/Services/events.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+
+interface Event {
+  Name: string;
+  Approved: boolean;
+  Contact: string;
+  Event_date: Date;
+  Location: string;
+  Phone_Number: string;
+  Poster_Image:string;
+  Price:string;
+  Website:string;
+  Community_Sponsor: boolean;
+  Description: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  constructor(private firestore: AngularFirestore) { }
+
+  getEvents(): Observable<Event[]> {
+    return this.firestore.collection<Event>('Events',ref=>ref.orderBy('Community_Sponsor','desc')).valueChanges();
+  }
+}
--- romani-in-seattle/src/app/components/events/events.component.ts
+++ romani-in-seattle/src/app/components/events/events.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from 'src/app/Services/events.service';
+interface Event {
+  Name: string;
+  Approved: boolean;
+  Contact: string;
+  Event_date: Date;
+  Location: string;
+  Phone_Number: string;
+  Poster_Image:string;
+  Price:string;
+  Website:string;
+  Community_Sponsor: boolean;
+  Description: string;
+}
+
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
+  events: Event[] = [];
 
+  constructor(private eventService: EventsService) { }
+
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data;
+    });
+  }
 }
+
