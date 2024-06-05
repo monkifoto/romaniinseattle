74b84408708066a886107bf4afd89eb3fc121fd2
Monkifoto LLc <monkifoto@gmail.com>
2024-06-04 23:38:58 -0700

    added job model

--- romani-in-seattle/src/app/Model/job.model.ts
+++ romani-in-seattle/src/app/Model/job.model.ts
@@ -1,2 +1,8 @@
 export class Job {
+  Company_Name: string ='';
+  Contact_Name: string = '';
+  Phone_Number: string ='';
+  Email: string='';
+  Job_Description: string='';
+  Website: string='';
 }
--- romani-in-seattle/src/app/components/home/home.component.ts
+++ romani-in-seattle/src/app/components/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+interface ResourceLink {
+  name: string;
+  url: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  resourceLinks: ResourceLink[] = [
+    { name: 'Ambasada Romaniei in SUA', url: 'https://washington.mae.ro/' },
+    { name: 'American Romanian Cultural Society', url: 'www.arcsproject.org' }
+  ];
 
 }
